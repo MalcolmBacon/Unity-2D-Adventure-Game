defd7f9205384ed1cd7c3dd2b966067a812958c4
Malcolm Bacon <mbacon23@uwo.ca>
2020-11-16 16:17:47 -0500

    Added breakable pot animations and scripts

---
+++ Unity 2D Farming Simulator/Assets/__Scripts/Pot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pot : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SmashPot()
+    {
+        animator.SetBool("smash", true);
+        StartCoroutine(BreakCoroutine());
+    }
+
+    IEnumerator BreakCoroutine()
+    {
+        yield return new WaitForSeconds(0.26f);
+        this.gameObject.SetActive(false);
+    }
+}
